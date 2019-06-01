f460f7820b2190cfffbfd227cd9e1ea1736b8090
marcinzelent <zelent.marcin@gmail.com>
2019-06-01 17:07:58 +0200

    Set roles on the users

--- src/sql/roles.sql
+++ src/sql/roles.sql
@@ -1,3 +1,17 @@
+ALTER ROLE db_owner ADD MEMBER marcin;
+
+ALTER ROLE db_ddladmin ADD MEMBER gosia;
+ALTER ROLE db_datareader ADD MEMBER gosia;
+ALTER ROLE db_datawriter ADD MEMBER gosia;
+
+ALTER ROLE db_securityadmin ADD MEMBER paulius;
+ALTER ROLE db_accessadmin ADD MEMBER paulius;
+ALTER ROLE db_backupoperator ADD MEMBER paulius;
+
+ALTER ROLE db_ddladmin ADD MEMBER jakub;
+ALTER ROLE db_datareader ADD MEMBER jakub;
+ALTER ROLE db_datawriter ADD MEMBER jakub;
+
 CREATE ROLE reader AUTHORIZATION marcin;
 GRANT SELECT TO reader;
 ALTER ROLE reader ADD MEMBER getter;
