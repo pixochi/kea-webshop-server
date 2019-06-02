1ece1c281ddb21e300867006e97e9ad5019eba10
pixochi <kozak.jakub55@gmail.com>
2019-06-02 12:09:12 +0200

    Add a transaction for creating an order

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,8 +1,6 @@
 import {getConnection, Repository} from 'typeorm';
 import uuid4 from 'uuid';
 
-import OrderItemController from './order-item';
-
 import OrderEntity from 'src/entity/order';
 import Product from 'src/entity/product';
 import OrderItemEntity from 'src/entity/order-item';
@@ -25,8 +23,6 @@ export default class OrderController {
   }
 
   async createOrder(orderItems: any[], userId: string, country: string) {
-
-    const orderItemController = await new OrderItemController();
 
     const user = new User();
     user.id = userId;
@@ -56,18 +52,56 @@ export default class OrderController {
         return itemEntity;
     });
 
-    newOrder.items = orderItemEntities;
-    const savedOrder = await this.orderRepository.insert(newOrder);
+    // get a connection and create a new query runner
+    const connection = getConnection();
+    const queryRunner = connection.createQueryRunner();
 
-    const orderItemsPromises = orderItemEntities.map(item => {
-        return new Promise<OrderItemEntity>(async (resolve) => {
-            const savedItem = await orderItemController.createOrderItem(item);
+    // establish real database connection using our new query runner
+    await queryRunner.connect();
+
+    // let's now open a new transaction:
+    await queryRunner.startTransaction();
+
+    let savedOrder;
+
+    try {
+      // execute some operations on this transaction:
+      // newOrder.items = orderItemEntities;
+      savedOrder = await queryRunner.manager.save(newOrder);
+
+      const orderItemsPromises = orderItemEntities.map(item => {
+        return new Promise<OrderItemEntity>(async (resolve, reject) => {
+          const savedItem = await queryRunner.manager.query(`
+              DECLARE @T TABLE (
+                id     INT,
+                amount INT
+              );
+
+              INSERT INTO "order_item"("price", "amount", "orderId", "productId") OUTPUT
+              INSERTED."id",
+              INSERTED."amount"
+              INTO @T
+              VALUES (@0, @1, @2, @3);
+            `, [item.price, item.amount, item.order.id, item.product.id]
+          );
+            // const savedItem = await orderItemController.createOrderItem(item);
             resolve(savedItem);
         });
-    });
-    await Promise.all<OrderItemEntity>(orderItemsPromises);
+      });
 
-    return savedOrder;
+      try {
+        await Promise.all(orderItemsPromises);
+
+        // commit transaction now:
+        await queryRunner.commitTransaction();
+    } catch (error) {
+      // since we have errors lets rollback changes we made
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // you need to release query runner which is manually created:
+      await queryRunner.release();
+      return savedOrder;
+    }
   }
 
 }
